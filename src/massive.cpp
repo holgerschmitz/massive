49d9b97def658033783cce996466a6ab339bd265
Holger Schmitz <holger.schmitz@stfc.ac.uk>
2022-05-20 12:07:52 +0100

    Setting multiple build targets

--- src/massive.cpp
+++ src/massive.cpp
@@ -5,5 +5,11 @@
 int main()
 {
     std::cout << "Hello World!" << std::endl;
+#ifdef MASSIVE_1X_1V
+    std::cout << "Is 1x1v!" << std::endl;
+#endif    
+#ifdef MASSIVE_2X_3V
+    std::cout << "Is 2x3v!" << std::endl;
+#endif    
     return 0;
 }
